56931b399e2cc2e80e4eb232dc6cb4a143f375bb
Aditya Kamble <adityakamble6101@gmail.com>
2023-06-26 23:21:53 +0530

    init

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <SignIn />
+    </main>
+  );
+}
